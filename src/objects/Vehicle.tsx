6f745c49ba0747b6f9fa25c90bc657157c4bbbf3
Arlan Dantas <arlanjose@gmail.com>
2021-12-30 13:08:35 -0300

    enhance: Use vehicle position

--- src/objects/Vehicle.tsx
+++ src/objects/Vehicle.tsx
@@ -15,43 +15,45 @@ function Vehicle (props: Props) {
     tank: 'brown',
   };
 
+  const position = props.vehicle.getPosition();
+
   return (
     <>
       <Boundaries boundaries={props.vehicle.getBoundaries()}/>
       <g
-        transform={`rotate(${props.vehicle.getAngle()-90}, ${props.vehicle.getX() + (width/2)}, ${props.vehicle.getY() + (height/2)})`}
+        transform={`rotate(${props.vehicle.getAngle()-90}, ${position.x + (width/2)}, ${position.y + (height/2)})`}
       >
         <rect
-          x={props.vehicle.getX() ?? 10}
-          y={props.vehicle.getY() ?? 10}
+          x={position.x ?? 10}
+          y={position.y ?? 10}
           width={width}
           height={height}
           fill={colors.tank}
         />
         <rect
-          x={props.vehicle.getX() - 5}
-          y={props.vehicle.getY() + 5}
+          x={position.x - 5}
+          y={position.y + 5}
           width={10}
           height={height - 10}
           fill={colors.wheel}
         />
         <rect
-          x={props.vehicle.getX() + width - 5}
-          y={props.vehicle.getY() + 5}
+          x={position.x + width - 5}
+          y={position.y + 5}
           width={10}
           height={height - 10}
           fill={colors.wheel}
         />
         <circle
-          cx={props.vehicle.getX() + (width/2)}
-          cy={props.vehicle.getY() + (height/2)}
+          cx={position.x + (width/2)}
+          cy={position.y + (height/2)}
           r={10}
           fill={colors.gun}
         />
         <rect
-          transform={`rotate(${props.vehicle.getGunAngle()}, ${props.vehicle.getX() +  + (width/2)}, ${props.vehicle.getY() + (height/2)})`}
-          x={props.vehicle.getX() + (width/2) - 2.5}
-          y={props.vehicle.getY() + (height/2)}
+          transform={`rotate(${props.vehicle.getGunAngle()}, ${position.x +  + (width/2)}, ${position.y + (height/2)})`}
+          x={position.x + (width/2) - 2.5}
+          y={position.y + (height/2)}
           width={5}
           height={height - 15}
           fill={colors.gun}
