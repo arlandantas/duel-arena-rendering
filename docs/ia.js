fda45a52e0a863e8ffa0a14509bdc489c11ff142
Arlan Dantas <arlanjose@gmail.com>
2022-01-28 11:13:15 -0300

    feat: Test docs generation

---
+++ docs/ia.js
@@ -0,0 +1,68 @@
+import { Vehicle } from "duel-arena-engine";
+
+/**
+ * Move the vehicle ahead
+ * @param {number} speed - Number of pixels to move
+ * @return {void}
+ */
+function move(speed) { /* HERE GOES THE MAGIC */ }
+
+/**
+ * Move the vehicle ahead
+ * @param {number} speed - Number of pixels to move
+ * @return {void}
+ */
+function moveForward (speed) { /* HERE GOES THE MAGIC */ }
+
+/**
+ * Move the vehicle backward
+ * @param {number} speed - Number of pixels to move
+ * @return {void}
+ */
+function moveBackward (speed) { /* HERE GOES THE MAGIC */ }
+
+/**
+ * Rotate the vehicle clockwise
+ * @param {number} speed - Number of degrees to rotate
+ * @return {void}
+ */
+function rotateClockwise (speed) { /* HERE GOES THE MAGIC */ }
+
+/**
+ * Rotate the vehicle anti-clockwise
+ * @param {number} speed - Number of degrees to rotate
+ * @return {void}
+ */
+function rotateAnticlockwise (speed) { /* HERE GOES THE MAGIC */ }
+
+/**
+ * Rotate only the gun clockwise
+ * @param {number} speed - Number of degrees to rotate
+ * @return {void}
+ */
+function rotateGunClockwise (speed) { /* HERE GOES THE MAGIC */ }
+
+/**
+ * Rotate only the gun anti-clockwise
+ * @param {number} speed - Number of degrees to rotate
+ * @return {void}
+ */
+function rotateGunAnticlockwise (speed) { /* HERE GOES THE MAGIC */ }
+
+/**
+ * Get information about your own vehicle
+ * @return {Vehicle}
+ */
+function getVehicle () { /* HERE GOES THE MAGIC */ }
+
+/**
+ * Get information about all vehicles currently in the world
+ * @return {Vehicle[]}
+ */
+function getVehicles () { /* HERE GOES THE MAGIC */ }
+
+/**
+ * Shot a bullet
+ * @return {void}
+ */
+function fire () { /* HERE GOES THE MAGIC */ }
